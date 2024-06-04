1dfa1bfc0a9909d975c416b4fbb6b83b4c8c6171
6luv <jmj00005@naver.com>
2024-06-04 13:49:32 +0900

    fix: clean up import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import DailyWord from "./pages/DailyWord";
+// import DailyWord from "./pages/DailyWord";
 import AnotherDailyWord from "./pages/AnotherDailyWord";
 
 const App: FC = () => {
